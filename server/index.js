1e908a0abdec1e36d1a6afb9c4401141604f54e2
Simon Diep <simondiep1@gmail.co>
2019-02-13 12:01:22 -0600

    Serve index.html when requesting root path

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 3001;
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 initializeSocketIo(io);
 
