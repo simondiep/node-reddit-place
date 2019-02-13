809c766933ee9de76a912d01e59dc9ebe538b6d9
Simon Diep <simondiep1@gmail.co>
2019-02-13 11:25:13 -0600

    Simplify routing of resources

--- server/index.js
+++ server/index.js
@@ -9,19 +9,7 @@ const io = require('socket.io')(server);
 const PORT = process.env.PORT || 3001;
 const path = require('path');
 
-const router = express.Router();
-
-// root (/) should always serve our server rendered page
-router.use('^/$', serverRenderer);
-
-// other static resources should just be served as they are
-router.use(express.static(
-  path.resolve(__dirname, '..', 'build'),
-  { maxAge: '30d' },
-));
-
-// tell the app to use the above rules
-app.use(router);
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 initializeSocketIo(io);
 
